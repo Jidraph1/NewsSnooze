aad8bce60befd1437caa6a3cad0bbdd6b224b578
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-04-30 13:34:02 +0300

    Configuration classes

--- app/config.py
+++ app/config.py
@@ -1,6 +1,25 @@
-# class Config:
-#     """
-#     General confguration parent class
-#     """
-#     pass
+class Config:
+    """
+    General confguration parent class
+    """
+    pass
+
+class ProdConfig(Config):
+    """
+    Production cofiguration child class
+
+    Args:
+    Config: The parent confguration class with General confguration settings
+    """
+    pass
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    
+    Args:
+    Config: The parent configuration class with general configuration settings
+    """
+
+    Debug = True
 

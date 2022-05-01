ce7cacfe83e2462c1b730f7f197fe25c76771062
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-01 17:02:56 +0300

    added .json request

--- app/request.py
+++ app/request.py
@@ -6,3 +6,44 @@ from .models import articles
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 
+#Getting the News base Url
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_source():
+    """
+    Function that gets the json response to our Url request
+    """
+    get_source_url = base_url.format(api_key)
+    
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
+
+        source_results = None
+        if get_source_response['results']:
+            source_results_list = get_source_response['results']
+            source_results = process_results(source_result_list)
+
+            return source_results
+
+def process_results():
+    """
+    Function that processes the source result and tranforms them into a list of objects
+    Args:
+    source_list: A list of dictionaries that contain the source details
+    Returns: 
+    source_list: A list of source objects
+    """
+
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        title = source_item.get('title')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get ('category')
+
+        source_object=source.Source(id,name,description,url,category)
+        source_results.append(source_object)
+
+    return source_results
--- run.py
+++ run.py
@@ -2,3 +2,5 @@ from app import app
 
 if __name__ == '__main__':
     app.run()
+
+    # https://newsapi.org/v2/everything?q=Apple&from=2022-04-30&sortBy=popularity&apiKey=d344e4abb67f4eb9b70420c812b2c7c2

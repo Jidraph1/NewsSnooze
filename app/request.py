c04517462a62c61b69a2c04d5c407f854ab65827
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-01 23:22:54 +0300

    get article request

--- app/request.py
+++ app/request.py
@@ -46,3 +46,38 @@ def process_results(source_list):
 
     return source_results
 
+
+def get_article(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,apiKey)
+    print(article_source_url)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
+
+def process_articles_results(news):
+    '''
+    A function that processes the json files of news articles from api
+    '''
+    article_source_results = []
+    for article in news:
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        urlToImage = article.get('urlToImage')
+        publishedAt = article.get ('publishedAt')
+
+        if url:
+            article_objects = articles.Article(author,title,description,url,urlToImage,publishedAt)
+            article_source_results.append(article_objects)
+
+    return article_source_results
